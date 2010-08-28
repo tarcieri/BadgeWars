51c9781bc08c44ddcecddaad3879d3b22c1898d4
Tony Arcieri <tony@medioh.com>
2010-08-28 14:17:07 -0600

    Debugging printout

--- src/badgewars.c
+++ src/badgewars.c
@@ -1,17 +1,21 @@
 #include <badgewars.h>
+#include <stdio.h>
 
 /* Initialize the BadgeWars world */
 void bw_init(struct bw_world *world)
 {
+    puts("bw_init requested");
 }
 
 /* Run the BadgeWars world for a single instruction */
 int bw_run(struct bw_world *world)
 {
+    puts("bw_run requested");
     return 0;
 }
 
 /* Receive a BadgeWars command from the outside world */
 void bw_receive(struct bw_world *world, OPCODE command, void *addr, void(*send_response)(int, void *))
 {
+    printf("bw_receive got op: %d\n", command.op);
 }
